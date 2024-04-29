ace012df0566c82290225c7f95385f0ef862dbf8
Ashish Pratap <ashish.pratap@centrica.com>
2024-04-29 10:48:39 +0530

    JS Code for Input Validation

---
+++ JavaScripts/CaseForm.js
@@ -0,0 +1,28 @@
+var DNB = DNB || {};
+DNB.Case = {
+    Fields: {
+        ArrivalWindowStartTime: 'dnb_arrivalwindowstarttime',
+        ArrivalWindowEndTime: 'dnb_arrivalwindowendtime'
+    },
+
+    onLoad: function (executionContext) {
+        var formContext = executionContext.getFormContext();
+        var ctrlArrivalWindow = formContext.getControl(DNB.Case.Fields.ArrivalWindowStartTime);
+        ctrlArrivalWindow.addOnOutputChange(DNB.Case.onChangeEndTime);
+    },
+
+    onChangeEndTime: function (executionContext) {
+        var formContext = executionContext.getFormContext();
+        var ctrlArrivalWindow = formContext.getControl(DNB.Case.Fields.ArrivalWindowStartTime);
+        var validationResult = ctrlArrivalWindow.getOutputs();
+        var uniqueId = 'dnb_timewindowvalidation';
+        if (validationResult &&
+            validationResult[`${DNB.Case.Fields.ArrivalWindowStartTime}.fieldControl.isTimeWindowValid`] &&
+            validationResult[`${DNB.Case.Fields.ArrivalWindowStartTime}.fieldControl.isTimeWindowValid`].value === false) {
+            ctrlArrivalWindow.setNotification('You must specify an end time that happens after the start time.', uniqueId);
+        }
+        else {
+            ctrlArrivalWindow.clearNotification(uniqueId);
+        }
+    }
+}
